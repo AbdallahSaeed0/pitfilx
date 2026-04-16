b27c27683a69cadb8d257448a7cf64fd98121d12
AbdallahSaeed0 <bodyahly17@gmail.com>
2026-04-16 07:39:19 +0200

    update latest

--- Pitflix.API/obj/x64/Release/net8.0-windows/win-x64/Pitflix.API.AssemblyInfo.cs
+++ Pitflix.API/obj/x64/Release/net8.0-windows/win-x64/Pitflix.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pitflix.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9901608341eb5aaa4a1b0373a1cb27e1effd745")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3752045913dbadf193a7c52bd43e4fac02ca794")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pitflix.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pitflix.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
